4b35bb870e283412759d1c3a0e9b7b742908d65c
Raynomch97 <msiu@nd.edu>
2026-03-16 20:40:05 -0400

    fix image paths

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,31 +1,34 @@
-export default function Menu(){
-  return(
+import mango from './assets/mango.jpg'
+import matcha from './assets/matcha.jpg'
+import chocolate from './assets/chocolate.jpg'
+import strawberries from './assets/strawberries.jpg'
+
+export default function Menu() {
+  return (
     <section id="menu">
-      <h2 className = "menu-title">Our Flavors</h2>
+      <h2>Our Flavors</h2>
+
       <div className="menu-grid">
-
         <div className="menu-card">
-        <img src = "/mango.jpg"></img>
-          Mango Shaved <br/>
-          Snow
+          <img src={mango} alt="Mango shaved snow" />
+          Mango Shaved Snow
         </div>
 
         <div className="menu-card">
-        <img src = "/strawberries.jpg"></img>
-          Strawberry Shaved Snow
+          <img src={matcha} alt="Matcha shaved snow" />
+          Matcha Shaved Snow
         </div>
 
         <div className="menu-card">
-        <img src = "/matcha.jpg"></img>
-          Matcha Shaved <br/>
-          Snow
-        </div>
-
-        <div className="menu-card">
-        <img src = "/chocolate.jpg"></img>
+          <img src={chocolate} alt="Chocolate shaved snow" />
           Chocolate Shaved Snow
+        </div>
+
+        <div className="menu-card">
+          <img src={strawberries} alt="Strawberries shaved snow" />
+          Strawberries Shaved Snow
         </div>
       </div>
     </section>
-  );
+  )
 }
