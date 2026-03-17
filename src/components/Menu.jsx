e07dba578b4ecba813aa4d5abfc07736dbaf58f5
Raynomch97 <msiu@nd.edu>
2026-03-16 20:43:24 -0400

    fix public image paths

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,31 +1,25 @@
-import mango from './assets/mango.jpg'
-import matcha from './assets/matcha.jpg'
-import chocolate from './assets/chocolate.jpg'
-import strawberries from './assets/strawberries.jpg'
-
 export default function Menu() {
   return (
     <section id="menu">
       <h2>Our Flavors</h2>
-
       <div className="menu-grid">
         <div className="menu-card">
-          <img src={mango} alt="Mango shaved snow" />
+          <img src={`${import.meta.env.BASE_URL}mango.jpg`} alt="Mango shaved snow" />
           Mango Shaved Snow
         </div>
 
         <div className="menu-card">
-          <img src={matcha} alt="Matcha shaved snow" />
+          <img src={`${import.meta.env.BASE_URL}matcha.jpg`} alt="Matcha shaved snow" />
           Matcha Shaved Snow
         </div>
 
         <div className="menu-card">
-          <img src={chocolate} alt="Chocolate shaved snow" />
+          <img src={`${import.meta.env.BASE_URL}chocolate.jpg`} alt="Chocolate shaved snow" />
           Chocolate Shaved Snow
         </div>
 
         <div className="menu-card">
-          <img src={strawberries} alt="Strawberries shaved snow" />
+          <img src={`${import.meta.env.BASE_URL}strawberries.jpg`} alt="Strawberries shaved snow" />
           Strawberries Shaved Snow
         </div>
       </div>
